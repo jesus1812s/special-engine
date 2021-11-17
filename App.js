3bdcf81082a35b2cf5a7e736c56e690263d5a68b
jesus1812s <jesussalasgil@gmail.com>
2021-11-17 10:49:03 -0400

    Uploading how to display data from api

--- App.js
+++ App.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View } from 'react-native';
+import axios from 'axios';
+
+const getPopularMovies = async () => {
+  const resp = await axios.get(
+    'https://api.themoviedb.org/3/movie/550?api_key=47c89e774e4c9b2e2f1c1e3947e16cb2'
+  );
+  //This console log has been added to check the response from the API
+  console.log(JSON.stringify(resp.data, null, 2));
+  return resp.data;
+};
 
 const App = () => {
+  const [movie, setMovie] = useState('');
+  getPopularMovies().then((data) => setMovie(data));
   return (
     <View
       style={{
@@ -10,7 +22,9 @@ const App = () => {
         alignItems: 'center',
       }}
     >
-      <Text>Hello, world!</Text>
+      <Text>Movie Name: {movie.title}</Text>
+      <Text>Language: {movie.original_language}</Text>
+      <Text>Realease: {movie.release_date}</Text>
     </View>
   );
 };
